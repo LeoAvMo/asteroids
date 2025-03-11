0d60457ba5130ec69234f905590eb7de26162797
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-03-11 09:54:44 -0600

    Initialized pygame screen in main.py and added pycache to .gitignore

--- main.py
+++ main.py
@@ -2,12 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 
 def main():
+    ## Intro screen
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    #Initializing the game
+    pygame.init()
+    backgroundColor = (0,0,0)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill(backgroundColor)
+        pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
