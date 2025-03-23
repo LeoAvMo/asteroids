2fe26bbb537238ab42c771bea9ac72c88cc49018
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-03-22 20:54:27 -0600

    Corrected circleshape, added constant, player in main, created player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     ## Intro screen
@@ -12,19 +13,28 @@ def main():
     
     #Initializing the game
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     backgroundColor = (0,0,0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Main loop
     while True:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # Update game state
+        player.update(dt)
+        
         screen.fill(backgroundColor)
+        player.draw(screen)
+        
         pygame.display.flip()
+        
         dt = clock.tick(60)/1000
         print(f"Delta Time: {dt}")
         
